4a1f87935619e0f22a3d352fe00017fc9b2b33e4
Vasanth9384 <Vasanth9384@gmail.com>
2023-07-16 10:22:51 +0530

    publish to github

---
+++ assets/scripts/app.js
@@ -0,0 +1,22 @@
+const modalEleRef = document.getElementById('add-modal');
+const showModalbtnRef = document.getElementById('showModal');
+const closeModalbtnRef = modalEleRef.querySelector('#close-modal');
+const backdropEleRef = document.getElementById('backdrop');
+
+const toggleBackdrop = () => {
+    backdropEleRef.classList.toggle('visible');
+}
+
+const toggleModalHandler = (e) => {
+    modalEleRef.classList.toggle('visible');
+    toggleBackdrop();
+}
+ 
+
+showModalbtnRef.addEventListener('click', toggleModalHandler);
+
+closeModalbtnRef.addEventListener('click', toggleModalHandler);
+
+
+
+backdropEleRef.addEventListener('click', toggleModalHandler)
